07bbee0548a9308df84fa66dd99e08dea8184806
phzsantos <phzsantos2002@gmail.com>
2021-03-14 16:41:30 -0300

    Conversão explicita

--- 8-Conversões.cpp
+++ 8-Conversões.cpp
@@ -13,6 +13,11 @@ int main(){
 	// Conversão implicita
 	numero = novo; // Implicita porque você não indica nada, apenas declara.
 
+	// Conversão explicita
+	pi = int(pi); // Explicita porque você está indicando explicitamente para qual tipo quer converter.
+
+	numero = static_cast<double>(pi); // Outra forma explicita
+
 	std::cout << "\nEste é valor de pi: " << pi << '\n';
 	std::cout << "Este é valor de numero: " << numero << '\n';
 	std::cout << "Este é valor de novo: " << novo << '\n';
