e736949f2100cb183967ed4416e70d10ad781805
phzsantos <phzsantos2002@gmail.com>
2021-03-16 22:20:01 -0300

    Input com string

--- 10-Entradas.cpp
+++ 10-Entradas.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
 int main(){
-	const int total {99};
-	char letra[ total ];
-	std::cout << "Digite uma letra: ";
+	
+	std::string texto;
+
+	//const int total {99};
+	//char letra[ total ];
+	std::cout << "Digite um texto: ";
 
 	//std::cin >> letra; caso você use a variavel com array e deixe o std::cin assim, ele vai pegar tudo ate o primeiro espaço.
 	// Por exemplo se você desse um input de paulo henrique, ele só consideraria paulo
 
-	std::cin.getline( letra, total);
+	//std::cin.getline( letra, total);
 	// Essa é a forma certa dele usando array, pois aqui ele considera ate os espaços.
 
-	std::cout << "A letra que você digitou foi: " << letra << '\n'; 
+	std::getline( std::cin , texto);
+	// Essa é a forma de pegar um input inteiro no formato de string. 
+
+	std::cout << "A texto que você digitou foi: " << texto << '\n'; 
 }
