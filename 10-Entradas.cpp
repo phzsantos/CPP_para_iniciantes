d67445a9c9027b639ddffaa981df1fc48d01f19e
phzsantos <phzsantos2002@gmail.com>
2021-03-16 22:14:45 -0300

    Inputs com arrays

--- 10-Entradas.cpp
+++ 10-Entradas.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
 int main(){
-	char letra;
+	const int total {99};
+	char letra[ total ];
 	std::cout << "Digite uma letra: ";
-	std::cin >> letra;
+
+	//std::cin >> letra; caso você use a variavel com array e deixe o std::cin assim, ele vai pegar tudo ate o primeiro espaço.
+	// Por exemplo se você desse um input de paulo henrique, ele só consideraria paulo
+
+	std::cin.getline( letra, total);
+	// Essa é a forma certa dele usando array, pois aqui ele considera ate os espaços.
+
 	std::cout << "A letra que você digitou foi: " << letra << '\n'; 
 }
