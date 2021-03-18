590ec7d1373e4bbe17e5d7763f73834eee449250
phzsantos <phzsantos2002@gmail.com>
2021-03-17 22:28:58 -0300

    Usandoswitch

--- 11-Condições.cpp
+++ 11-Condições.cpp
@@ -7,16 +7,36 @@ int main(){
 	cout << "Me diga um numero! ";
 	cin >> (var);
 
+
+	// Condiconal (Usado para proposito geral)
 	if( var == 30 ){
 		cout << "O valor de var é 30" << '\n';
 	}
 
 	else if( var == 31){
-		cout << "O valor de var na verdade é 31" << '\n';
+		cout << "O valor de var é 31" << '\n';
 	}
 
 	else{
 		cout << "var é diferente de 30 e 31, var na verdade é: " << var << '\n';
 	}
+
+
+
+	//Switch (Usado apenas para comparações exatas)
+	switch( var ){
+		case 30:
+			cout << "O valor de var é 30!" << '\n';
+			break;
+
+		case 31:
+			cout << "O valor de var é 31!" << '\n';
+			break;
+
+		default:
+			cout << "Você digitou uma letra/numero que não é 30 nem 31." << '\n';
+			break;
+	}
+
 	return 0;
 }
