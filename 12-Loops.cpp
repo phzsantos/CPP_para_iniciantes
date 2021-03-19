7dcec577825399c0d5e4c93c267d75bad3508765
phzsantos <phzsantos2002@gmail.com>
2021-03-18 23:39:28 -0300

    For; For com Array; For dentro de for

--- 12-Loops.cpp
+++ 12-Loops.cpp
@@ -2,7 +2,7 @@
 
 
 int main(){
-
+	/*
 	// While
 	int numero = {1};
 
@@ -10,9 +10,9 @@ int main(){
 		std::cout << "Esse número é o " << numero << '\n';
 		numero++; // incrementa, equivale a: numero = numero + 1; ou seja adiciona 1.
 	}
+	*/
 
-	
-
+	/*
 	// Do while
 	numero = {1};
 
@@ -20,6 +20,41 @@ int main(){
 		std::cout << "Esse número é o " << numero << '\n';
 		numero++;
 	}while (numero <= 20);
+	*/
 
+
+	// For
+	for ( int i = {1}; i <= 10; i++){
+		std::cout << "Esse número é o " << i << '\n';
+	}
+
+
+
+	// For com array
+	std:: string cantores[] = {"Chester Bennington", "Chris Cornell", "Bruce Dickinson", "James Hatfield"};
+	int numeros[] = {11, 22, 33, 44, 55};
+
+	int total = sizeof( cantores ) / sizeof( cantores[0] ); // Deixa o tamanho do array dinamico
+
+	for ( int i2 = 0; i2 < total; i2++){ // Declara variavel/da onde ele começa; condição; incrementa.
+		std::cout << "O " << i2 << " é " << cantores[i2] << '\n';
+	}
+
+
+
+	// For dentro de For (Constroi uma piramide, não entendi muito bem.)
+	for( int i = 0; i < 8; i++ ){
+
+    	for( int k = 0; k < i + 1; k++ ){
+        	std::cout << "*";
+    	}
+
+    	for( int j = 8; j > 0; j-- ){
+      		std::cout << " ";
+   		}
+
+    	std::cout << '\n';
+
+	}
 	return 0;
 }
