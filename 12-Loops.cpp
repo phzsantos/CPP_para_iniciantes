1b034bf6587799348fa949c9b3addfebe90b391b
phzsantos <phzsantos2002@gmail.com>
2021-03-18 23:12:00 -0300

    Usando loop do while

--- 12-Loops.cpp
+++ 12-Loops.cpp
@@ -2,12 +2,24 @@
 
 
 int main(){
+
+	// While
 	int numero = {1};
 
 	while (numero <= 10){
 		std::cout << "Esse número é o " << numero << '\n';
 		numero++; // incrementa, equivale a: numero = numero + 1; ou seja adiciona 1.
 	}
+
+	
+
+	// Do while
+	numero = {1};
+
+	do {
+		std::cout << "Esse número é o " << numero << '\n';
+		numero++;
+	}while (numero <= 20);
 
 	return 0;
 }
