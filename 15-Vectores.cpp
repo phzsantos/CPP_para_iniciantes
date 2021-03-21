b3a519281fc597c9724daa697d8903c93324e57b
phzsantos <phzsantos2002@gmail.com>
2021-03-21 20:54:09 -0300

    Retorno o primeiro e o ultimo valor do vector

---
+++ 15-Vectores.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+	std::vector<std::string> vet = {"Python","Javascript","SQL","C++"};
+	std::vector<std::string>::iterator inicio = vet.begin();
+	std::cout << "Esse é o primeiro valor " << *inicio << '\n';
+	
+	vet.pop_back();
+	std::vector<std::string>::iterator fim = vet.end();
+	std::cout << "Esse é o ultimo valor " << *fim << '\n';
+	
+	
+	return 0;
+}
