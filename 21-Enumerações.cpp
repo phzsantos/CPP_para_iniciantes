6e77c6eca31daaef1d2b559fe294b702249c6c86
phzsantos <phzsantos2002@gmail.com>
2021-03-28 22:54:37 -0300

    enum cores com atribuição de valor no meio

--- 21-Enumerações.cpp
+++ 21-Enumerações.cpp
@@ -6,10 +6,20 @@ enum Cores {
 	preto
 };
 
+enum Cores2 {
+	branco,
+	verde = 50,
+	marrom
+};
+
 int main(int argc, char const ** argv){
 	Cores cores;
+	Cores2 cores2;
 	std::cout << "O numero da cor vermelha é: " << vermelho << '\n';
 	std::cout << "O numero da cor azul é: " << azul << '\n';
 	std::cout << "O numero da cor preta é: " << preto << '\n';
+	std::cout << '\n' << "O numero da cor branca é: " << branco << '\n';
+	std::cout << "O numero da cor verde é: " << verde << '\n';
+	std::cout << "O numero da cor marrom é: " << marrom << '\n';
 	return 0;
 }
