eea796b184181885207ddd7a778ad8a4ace2ed63
phzsantos <phzsantos2002@gmail.com>
2021-03-29 22:11:37 -0300

    Read + função de contar linhas

--- 22-Read.cpp
+++ 22-Read.cpp
@@ -4,12 +4,28 @@
 
 int main(int argc, char const ** argv ){
 	std::string linha;
+	bool numero {false};
+	std::string parametro;
+	int i = {1};
+
+	if( argc > 1){
+		parametro = argv [1];
+		if( parametro == "--numero" || parametro == "-n" ){
+			numero = true;
+		}
+	}
+
 
 	std::ifstream arquivo("22.2-Arquivo.txt");
 
 	if( arquivo.is_open() ){
 		while( getline( arquivo, linha ) ){
-			std::cout << linha << '\n';
+			if( numero ){
+				std::cout << i << " \u2502 " << linha << '\n';
+				i++;
+			}else{
+				std::cout << linha << '\n';
+			}
 		}
 
 		arquivo.close();
