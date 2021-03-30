0d9e3db2ad2652047a71742c2df521470a549e35
phzsantos <phzsantos2002@gmail.com>
2021-03-29 22:21:17 -0300

    Escrevendo no arquivo, sobrescrevendo

---
+++ 22.1-Write.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char const ** argv ){
+
+	std::string conteudo, outro;
+	std::ofstream arquivo;
+
+	arquivo.open("22.2-Arquivo.txt");
+
+	std::cout << "Escreva algo para adicionar no arquivo: ";
+	std::getline( std::cin, conteudo);
+	std::cout << "Escreva mais alguma coisa para adicionar no arquivo: ";
+	std::getline( std::cin, outro);
+
+	arquivo << conteudo << '\n';
+	arquivo << outro << '\n';
+
+	arquivo.close();
+
+	std::cout << "Dados gravados!!!" << '\n';
+	return 0;
+}
