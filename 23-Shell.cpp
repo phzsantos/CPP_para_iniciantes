6c26af685f7bf347f53472154a23d57b69eba102
phzsantos <phzsantos2002@gmail.com>
2021-03-30 20:54:00 -0300

    Função system para executar comandos no shell

--- 23-Shell.cpp
+++ 23-Shell.cpp
@@ -2,10 +2,13 @@
 #include <cstdlib>
 
 int	main(int argc, char const ** argv ){
+	system("clear");
 	std::string home = getenv("HOME");
 	std::string shell = getenv("SHELL");
 
 	std::cout << "O caminho da sua home é: " << home << '\n';
 	std::cout << "O seu shell padrão é o: " << shell << '\n';
+	std::cout << '\n' << "Isso é o que o comando ls faz:" << '\n';
+	system("ls");
 	return 0;
 }
