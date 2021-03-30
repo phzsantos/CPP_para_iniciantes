a2d5ca4c7fb465ddca2d87b491bb29ca8870018c
phzsantos <phzsantos2002@gmail.com>
2021-03-30 20:50:00 -0300

    Usando getenv para pegar o shell padrão

--- 23-Shell.cpp
+++ 23-Shell.cpp
@@ -3,7 +3,9 @@
 
 int	main(int argc, char const ** argv ){
 	std::string home = getenv("HOME");
+	std::string shell = getenv("SHELL");
 
 	std::cout << "O caminho da sua home é: " << home << '\n';
+	std::cout << "O seu shell padrão é o: " << shell << '\n';
 	return 0;
 }
