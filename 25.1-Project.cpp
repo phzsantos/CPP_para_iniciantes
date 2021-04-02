f9b0aee9878ea6a9cd51c2a4cd2f294bc9f09de6
phzsantos <phzsantos2002@gmail.com>
2021-04-01 22:01:35 -0300

    Construindo as condicionais e a entrada

--- 25.1-Project.cpp
+++ 25.1-Project.cpp
@@ -4,12 +4,32 @@ void _help(){
 	system("clear");
 	std::cout << "::::::: HELP :::::::\n"
 		     "\nPara iniciar o programa:\n"
-		     "use: ./a.out [parametro] [numero]\n"
+		     "use: ./a.out [--parametro] [numero]\n"
 		     "\nParametros:\n"
 	       	     "--dec2bin para converter decimal para binario\n"
 	     	     "--bin2dec para converter binario para decimal\n";	     
 }
 
+int dec2bin( int number ){
+	std::cout << "Eu sou o dec2bin" << number << '\n';
+}
+
+int bin2dec( int number ){
+	std::cout << "Eu sou o bin2dec" << number << '\n';
+}
+
 void _start( int argc, char ** argv ){	
-	_help();
+	if( argc > 2){
+		std::string param = argv[1];
+		int number = std::stoi( argv[2] );
+		if(param == "--dec2bin" || param == "-b" ){
+			dec2bin( number );
+		}else if( param == "--bin2dec" || param == "-d" ){
+			bin2dec( number );
+		}else{
+			_help();
+		}
+	}else{
+		_help();
+	}
 }
