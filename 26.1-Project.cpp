e08bf32911dc4ed0afa9106315c9b1827094c417
phzsantos <phzsantos2002@gmail.com>
2021-04-05 22:57:47 -0300

    Criando linha horizontal do desenho

--- 26.1-Project.cpp
+++ 26.1-Project.cpp
@@ -3,7 +3,15 @@
 int main(int argc, char const ** argv ){
   for( int i = 0; i <= 10; i++){
     if(i == 0 || i == 10){
-      std::cout << "+" << '\n';
+      std::cout << "+";
+      for( int k = 0; k <= 20; k++){
+        if(k == 20){
+          std::cout << "+";
+        }else{
+          std::cout << "-";
+        }
+      }
+      std::cout << '\n';
     }else{
       std::cout << "|" << '\n';
     }
